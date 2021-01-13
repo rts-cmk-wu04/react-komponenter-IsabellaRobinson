f80526f9a93c7cd3cd1b6049908440d287639542
Isabella Robinson <134569@rts-365.dk>
2021-01-13 13:32:51 +0100

    outdoor component

---
+++ src/components/outdoor.js
@@ -0,0 +1,22 @@
+import "./outdoor.css"
+
+function Outdoor() {
+    return(
+    <div id="outdoorCard">
+        <div id="headlinepic">
+        <a href="https://placeholder.com/"><img src="https://via.placeholder.com/293x375" alt="billede"/></a>
+        </div>
+    <h1 id="toplineOutdoor">Outdoor Experience</h1>
+    <p id="textOutdoor">It’s windy. The cool breeze of the ocean. It gives, a sense of beauty, in motion. 
+    All is flowing, rushing and tide-And I sit in wonder, dreaming beside.
+    </p>
+    <div id="minipics"> 
+    <a href="https://placeholder.com/"><img src="https://via.placeholder.com/100x110" alt="billede"/></a>
+    <a href="https://placeholder.com/"><img src="https://via.placeholder.com/100x110" alt="billede"/></a>
+    <a href="https://placeholder.com/"><img src="https://via.placeholder.com/100x110" alt="billede"/></a>
+    </div>
+</div>
+)
+
+}
+export default Outdoor;

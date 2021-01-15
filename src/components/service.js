45055db062b87f8e01e65a5c02e28c78051128b4
Isabella Robinson <134569@rts-365.dk>
2021-01-15 10:59:39 +0100

    Service component

---
+++ src/components/service.js
@@ -0,0 +1,17 @@
+import "./service.css"
+
+function Service(){
+    return(
+        <div id="serviceCard">
+            <a href="_blank ">
+                <img src="./Group 4.png" alt="billede"/>
+            </a>
+
+            <h1 id="toplineService">COSTOMER SERVICE</h1>
+            <p id="textService">lorem ipsum dolor sit amet, comsectetur adlipscing elit. Aliquam Aliquamdiam sit</p>
+        </div>
+    )
+    
+}
+export default Service;
+
